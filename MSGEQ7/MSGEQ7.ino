6a69bf3b00eb70dc6484db9aae55b1721cb89202
Simon Waldherr <git@simon.waldherr.eu>
2012-05-24 01:03:23 +0200

    update MSGEQ7

--- MSGEQ7/MSGEQ7.ino
+++ MSGEQ7/MSGEQ7.ino
@@ -12,53 +12,57 @@
   License: MIT
 */
 
-int analogPin = 0;    // read from multiplexer using analog input 0
-int strobePin = 4;    // strobe is attached to digital pin 4
-int resetPin = 5;     // reset is attached to digital pin 5
-int spectrumValue[7]; // array to store analog values
+int AnalogIn = 0;    // read from multiplexer using analog input 0
+int NextBand = 4;    // strobe (next band) is attached to digital pin 4
+int ResetPin = 5;    // reset is attached to digital pin 5
+int Spectrum[7];     // array to store analog values
 
 
 void setup()
   {
 
     // init IO
-    delay(100);
-    pinMode(analogPin, INPUT);
-    pinMode(strobePin, OUTPUT);
-    pinMode(resetPin, OUTPUT);
+    delay(250);
+    pinMode(AnalogIn, INPUT);
+    pinMode(NextBand, OUTPUT);
+    pinMode(ResetPin, OUTPUT);
     analogReference(DEFAULT);
-    digitalWrite(resetPin, LOW);
-    digitalWrite(strobePin, HIGH);
+    digitalWrite(ResetPin, LOW);
+    digitalWrite(NextBand, HIGH);
 
     // init Serial
-    delay(100);
+    delay(250);
     Serial.begin(115200);
-    Serial.println("_______________________");
-    Serial.println("MSGEQ7 Frequenz Analyse");
-    Serial.println("-----------------------");
-    Serial.println("   powerd by Arduino   ");
-    Serial.println("-----------------------");
-
+    Serial.println("|______________________________|");
+    Serial.println("|            MSGEQ7            |");
+    Serial.println("|------------------------------|");
+    Serial.println("| Seven Band Graphic Equalizer |");
+    Serial.println("|------------------------------|");
+    Serial.println("|     on an Arduino Shield     |");
+    Serial.println("|------------------------------|");
+    Serial.println("|      with Serial output      |");
+    Serial.println("|------------------------------|");
     // wait
     delay(5000);
   }
 
 void loop()
   {
-    digitalWrite(resetPin, HIGH);                          // reset band to 63 Hz
-    digitalWrite(resetPin, LOW);
+    digitalWrite(ResetPin, HIGH);                          // reset band to 63 Hz
+    digitalWrite(ResetPin, LOW);
 
     for (int i = 0; i < 7; i++)                            // for loop to save values to array
       {
-        digitalWrite(strobePin, LOW);
-        delayMicroseconds(25);                             // to allow the output to settle
-        spectrumValue[i] = doMath(analogRead(analogPin));  // read actual value to array
-        digitalWrite(strobePin, HIGH);                     // select next band (160Hz, 400Hz, 1000Hz, 2500Hz, 6250Hz, 16kHz)
+        digitalWrite(NextBand, LOW);
+        delayMicroseconds(22);                             // to allow the output to settle
+        Spectrum[i] = doMath(analogRead(AnalogIn));        // read actual value to array
+        digitalWrite(NextBand, HIGH);                      // select next band (160Hz, 400Hz, 1000Hz, 2500Hz, 6250Hz, 16kHz)
+        delayMicroseconds(3);
       }
 
     for (int i = 0; i < 7; i++)                            // for loop to print array to Serial
       {
-        if (spectrumValue[i] < 10 )                        // ignore values lower than 100 (you also can use doMath())
+        if (Spectrum[i] < 10 )                             // ignore values lower than 100 (you also can use doMath())
           {
             Serial.print("\t");
             Serial.print("0");
@@ -66,7 +70,7 @@ void loop()
         else
           {
             Serial.print("\t");
-            Serial.print(spectrumValue[i]);
+            Serial.print(Spectrum[i]);
           }
       }
     Serial.println();                                      // Serial new line
